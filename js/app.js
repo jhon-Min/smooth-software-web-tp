cff9fc3f8d7de7515f37496b2eeb54812b25340a
seojun <minnyisay@gmail.com>
2021-12-14 12:26:50 +0630

    finish animation for home page

--- js/app.js
+++ js/app.js
@@ -9,5 +9,25 @@ $(document).ready(function () {
     } else {
       $(".navbar").removeClass("site-nav");
     }
+
+    if (current > screenHeight) {
+      $("#toTop").removeClass("d-none");
+    } else {
+      $("#toTop").addClass("d-none");
+    }
   });
+});
+
+$(".navbar-toggler").click(function () {
+  let result = $(".navbar-collapse").hasClass("hello");
+
+  if (result) {
+    $(".menu-icon").removeClass("feather-menu").addClass("feather-x");
+    $(".navbar").addClass("shadow");
+    $(".navbar-collapse").removeClass("hello");
+  } else {
+    $(".menu-icon").removeClass("feather-x").addClass("feather-menu");
+    $(".navbar").removeClass("shadow");
+    $(".navbar-collapse").addClass("hello");
+  }
 });
